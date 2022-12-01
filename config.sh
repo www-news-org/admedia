636e170c5c13b310581366b57162a92876accc7b
MaliekBlog <112651078+www-news-org@users.noreply.github.com>
2022-12-01 20:26:42 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,1 +1,5 @@
+# Create the runner and start the configuration experience
 ./config.sh --url https://github.com/www-news-org/admedia --token A23OWRQIYJHOUZRHK6GWVZTDRD6Y4
+
+# Last step, run it!
+$ ./run.sh
